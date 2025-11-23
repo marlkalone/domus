e0af91a13ad3d1a589ca81a78e755e60b448cb85
Karl Malone <karlmalonex@gmail.com>
2025-11-22 23:13:28 -0300

    fix: ssl

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,19 +1,21 @@
 import * as path from "path";
 
 const isTest = process.env.NODE_ENV === "test";
+const isProduction = process.env.NODE_ENV === "production";
 
 export default () => ({
-  database: {
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    database: isTest
-      ? process.env.DATABASE_NAME_TEST || "domus-db-test"
-      : process.env.DATABASE_NAME,
-    synchronize: isTest,
-    logging: ["warn", "error"],
-    entities: [path.resolve(__dirname, "..", "**", "*.entity.{ts,js}")],
-    migrations: [path.resolve(__dirname, "..", "migrations", "*.{ts,js}")],
-    migrationsTableName: "migrations_typeorm",
-    migrationsRun: true,
-  },
+    database: {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+            ssl: isProduction ? { rejectUnauthorized: false } : false,
+        database: isTest
+            ? process.env.DATABASE_NAME_TEST || "domus-db-test"
+            : process.env.DATABASE_NAME,
+        synchronize: isTest,
+        logging: ["warn", "error"],
+        entities: [path.resolve(__dirname, "..", "**", "*.entity.{ts,js}")],
+        migrations: [path.resolve(__dirname, "..", "migrations", "*.{ts,js}")],
+        migrationsTableName: "migrations_typeorm",
+        migrationsRun: true,
+    },
 });
